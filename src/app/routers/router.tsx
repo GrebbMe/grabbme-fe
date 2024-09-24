6e48aeb8a159ba45f21a68da0d04d984fbb33f58
ParkJeongJoon <parkjeong616@gmail.com>
2024-09-24 19:12:04 +0900

    chore : routing path enum 수정

--- src/app/routers/router.tsx
+++ src/app/routers/router.tsx
@@ -3,17 +3,22 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Layout } from '@/app/layout';
 import { GithubLogin, SignUp } from '@/pages/auth/ui';
 
+const enum RouterPath {
+  GithubLogin = 'github-login',
+  SignUp = 'sign-up',
+}
+
 export const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
     children: [
       {
-        path: 'github-login',
+        path: RouterPath.GithubLogin,
         element: <GithubLogin />,
       },
       {
-        path: 'sign-up',
+        path: RouterPath.SignUp,
         element: <SignUp />,
       },
     ],
