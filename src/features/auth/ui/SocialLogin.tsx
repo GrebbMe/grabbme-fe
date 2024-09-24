fc4ab98c6703dea1498a6ee03c25b872dbaac150
ChanGyun Lee <lgyn10@naver.com>
2024-09-24 16:15:02 +0900

    chore : 불필요한 import 문 제거

--- src/features/auth/ui/SocialLogin.tsx
+++ src/features/auth/ui/SocialLogin.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const SocialLogin = () => {
   return <div>SocialLogin</div>;
 };
--- src/widgets/auth/ui/SignUpForm.tsx
+++ src/widgets/auth/ui/SignUpForm.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const SignUpForm = () => {
   return <div>SignUpForm</div>;
 };
