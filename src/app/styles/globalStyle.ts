62b8d7d04e4456911e05c72e045ca09ea3a5bd13
ParkJeongJoon <parkjeong616@gmail.com>
2024-09-26 14:35:11 +0900

    refactor : globalstyle 수정

--- src/app/styles/globalStyle.ts
+++ src/app/styles/globalStyle.ts
@@ -4,23 +4,24 @@ import reset from 'styled-reset';
 import Pretendard from '@/shared/assets/font/PretendardVariable.woff2';
 
 export const GlobalStyle = createGlobalStyle`
-  ${reset}
+
   @font-face {
     font-family: 'Pretendard';
     src: local('Pretendard'), url(${Pretendard}) format('woff2');
     font-weight: 100 900;
   }
-  html,body,#root {
-    width: 100%;
-    height: 100%;
-    
-  }
+  
   html{
     font-size:62.5%;
   }
-
+  *{
+        box-sizing: border-box;
+    }
   body {
-    font-family: 'Pretendard'
+    font-family: 'Pretendard';
+    padding: 0;
+    margin: 0;
   }
+ 
   
 `;
