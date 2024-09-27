078caba5c0aa75594d8245bf89f52fa2a4daeb8e
onejuneee <onejuneee721@gmail.com>
2024-09-27 19:22:29 +0900

    chore : storybook 환경에서도 themeprovider를 사용할 수 있도록 설정

---
+++ .storybook/preview.tsx
@@ -0,0 +1,24 @@
+import type { Preview } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { theme, GlobalStyle } from '@/app/styles';
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Story />
+        </ThemeProvider>
+    ),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
