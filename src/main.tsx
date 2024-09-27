a4decc084ce789b9a947e02b871f8d87b5b741a9
onejuneee <onejuneee721@gmail.com>
2024-09-27 18:50:18 +0900

    style : prettier와 lint 설정에 맞게 코드 재정렬

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client';
 import App from '@/app/App';
 import '@index.css';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
