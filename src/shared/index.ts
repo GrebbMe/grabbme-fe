3c2154c88a49323a60cd2e88714747aa47ee7ee3
PARKJEONGJOON <99483101+PARKJEONGJOON@users.noreply.github.com>
2024-09-27 13:11:57 +0900

    [HOTFIX] 폰트 파일 변경

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,1 +1,5 @@
+import PretendardMedium from '@/shared/assets/font/Pretendard-Medium.woff2';
+import PretendardRegular from '@/shared/assets/font/Pretendard-Regular.woff2';
+import PretendardSemibold from '@/shared/assets/font/Pretendard-Semibold.woff2';
 export { routerPath } from '@/shared/consts/rotuerPath';
+export { PretendardMedium, PretendardSemibold, PretendardRegular };
