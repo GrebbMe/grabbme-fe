7b89471a3a47eefdd6b59e8aa9ce14dcee06ecc8
onejuneee <onejuneee721@gmail.com>
2024-10-01 23:53:24 +0900

    fix : 사용하지 않는 variable 글꼴 제거

--- src/app/styles/globalStyle.ts
+++ src/app/styles/globalStyle.ts
@@ -34,7 +34,7 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
   body {
-      font-family: "Pretendard-Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+      font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     padding: 0;
     margin: 0;
   }
