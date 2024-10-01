aac09a2b6f244cdf608b1fca36409d91b49a4248
ChanGyunLee <lgyn10@naver.com>
2024-10-01 18:50:32 +0900

    test : 스토리북 테스트 코드 작성

---
+++ src/features/post/ui/deadlineCalendar/DeadlineCalendar.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import { DeadlineCalendar } from './DeadlineCalendar';
+
+const meta: Meta<typeof DeadlineCalendar> = {
+  title: 'component/features/DeadlineCalendar',
+  component: DeadlineCalendar,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof DeadlineCalendar>;
+
+const DeadlineCalendarWrapper = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+
+  return <DeadlineCalendar selectedDate={selectedDate} setSelectedDate={setSelectedDate} />;
+};
+
+export const DeadLineCalendar: Story = {
+  render: () => <DeadlineCalendarWrapper />,
+};
