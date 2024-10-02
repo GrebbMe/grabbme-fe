b18b93da0b1c93a2b854afae7dd6153cc7e9ea77
ChanGyunLee <lgyn10@naver.com>
2024-10-02 18:30:46 +0900

    refactor : forward ref를 사용했을 때 나타나는 오류로 인해 원복

--- src/features/post/ui/deadlineCalendar/CustomCalendarContainer.tsx
+++ src/features/post/ui/deadlineCalendar/CustomCalendarContainer.tsx
@@ -1,45 +1,47 @@
-import React, { forwardRef } from 'react';
 import { ButtonBoxStyle } from '@/features/post/ui/deadlineCalendar/CustomCalendarContainer.style';
 import { Button } from '@/shared/ui';
-import DatePicker from 'react-datepicker';
 
 interface CustomCalendarContainerProps {
-  datePickerRef: React.RefObject<DatePicker>;
+  datePickerRef: React.RefObject<any>;
   className?: string;
   children?: React.ReactNode;
-  setSelectedDate: (date: Date | null) => void;
-  tempDate: Date | null;
+  setSelectedDate: (date: Date | null) => void; // setSelectedDate 함수 받기
+  tempDate: Date | null; // 임시 선택된 날짜 받기
   selectedDate: Date | null;
   setTempDate: (date: Date | null) => void;
 }
 
-export const CustomCalendarContainer = forwardRef<HTMLDivElement, CustomCalendarContainerProps>(
-  (
-    { className, children, datePickerRef, setSelectedDate, selectedDate, setTempDate, tempDate },
-    ref,
-  ) => {
-    const handleCancel = () => {
-      if (datePickerRef.current) {
-        setTempDate(selectedDate);
-        datePickerRef.current.setOpen(false);
-      }
-    };
+// 커스텀 캘린더 컨테이너 컴포넌트
+export const CustomCalendarContainer = ({
+  className,
+  children,
+  datePickerRef,
+  setSelectedDate,
+  selectedDate,
+  setTempDate,
+  tempDate,
+}: CustomCalendarContainerProps) => {
+  const handleCancel = () => {
+    if (datePickerRef.current) {
+      setTempDate(selectedDate);
+      datePickerRef.current.setOpen(false);
+    }
+  };
 
-    const handleAccept = () => {
-      if (datePickerRef.current) {
-        setSelectedDate(tempDate);
-        datePickerRef.current.setOpen(false);
-      }
-    };
+  const handleAccept = () => {
+    if (datePickerRef.current) {
+      setSelectedDate(tempDate);
+      datePickerRef.current.setOpen(false);
+    }
+  };
 
-    return (
-      <div className={className} ref={ref}>
-        {children}
-        <ButtonBoxStyle>
-          <Button state={'default'} content={'취소'} size={'76px'} onClick={handleCancel} />
-          <Button state={'default'} content={'적용'} size={'76px'} onClick={handleAccept} />
-        </ButtonBoxStyle>
-      </div>
-    );
-  },
-);
+  return (
+    <div className={className}>
+      {children}
+      <ButtonBoxStyle>
+        <Button state={'default'} content={'취소'} size={'76px'} onClick={handleCancel} />
+        <Button state={'default'} content={'적용'} size={'76px'} onClick={handleAccept} />
+      </ButtonBoxStyle>
+    </div>
+  );
+};
