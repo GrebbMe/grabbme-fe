3ef8a2541d9162c241304bb38e717724c65b2e47
ParkJeongJoon <parkjeong616@gmail.com>
2024-10-04 23:41:02 +0900

    ui : 멀티플셀렉트 버튼

---
+++ src/shared/ui/button/AddButton.tsx
@@ -0,0 +1,14 @@
+import PlusIcon from '@/shared/assets/svg/IcPlus.svg?react';
+import { StyledButton } from '@/shared/ui/button/AddButton.style';
+
+interface AddButtonProps {
+  children: string;
+  onClick?: () => void;
+}
+
+export const AddButton = ({ children, onClick }: AddButtonProps) => (
+  <StyledButton onClick={onClick}>
+    {children}
+    <PlusIcon />
+  </StyledButton>
+);
--- src/shared/ui/button/RemoveButton.tsx
+++ src/shared/ui/button/RemoveButton.tsx
@@ -1,16 +1,14 @@
 import CloseIcon from '@/shared/assets/svg/IcClose.svg?react';
-import { StyledButton } from '@/shared/ui/button/RemoveStackButton.style';
+import { StyledButton } from '@/shared/ui/button/RemoveButton.style';
 
-interface SubButtonProps {
+interface RemoveButtonProps {
   children: string;
   onClick?: () => void;
 }
 
-const RemoveButton = ({ children, onClick }: SubButtonProps) => (
+export const RemoveButton = ({ children, onClick }: RemoveButtonProps) => (
   <StyledButton onClick={onClick}>
     {children}
     <CloseIcon />
   </StyledButton>
 );
-
-export default RemoveButton;
