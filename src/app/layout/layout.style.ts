90df23e4c5fd5e22474ac8094b4c21f979a43e58
ParkJeongJoon <parkjeong616@gmail.com>
2024-10-05 17:17:38 +0900

    ui : 레이아웃 배경색 추가

--- src/app/layout/layout.style.ts
+++ src/app/layout/layout.style.ts
@@ -21,6 +21,7 @@ const FooterContainer = styled.footer`
 const Container = styled.div`
   display: grid;
   margin: 0 auto;
+  background-color: ${({ theme }) => theme.color.BLUE_10};
   grid-template-columns: repeat(12, 8rem);
   column-gap: 2.4rem;
   max-width: 122.4rem;
@@ -29,14 +30,12 @@ const Container = styled.div`
     grid-template-columns: repeat(6, 1fr);
     gap: 2.4rem;
     margin: 2.4rem;
-    border: 1px solid blue;
   }
 
   ${({ theme }) => theme.device.MOBILE} {
     grid-template-columns: repeat(4, 1fr);
     gap: 1.6rem;
     margin: 1.6rem;
-    border: 1px solid red;
   }
 `;
 const GridContainer = styled.div`
