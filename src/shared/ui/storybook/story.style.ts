e947bc48dd126505ff425e46403c108e7a1f7983
onejuneee <onejuneee721@gmail.com>
2024-10-09 03:12:46 +0900

    ui : 효과적인 스토리북 테스트 환경을 위한 스타일 파일 추가

---
+++ src/shared/ui/storybook/story.style.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StoryContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const SectionContainer = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: flex-start;
+`;
+
+export const RowContainer = styled.li`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: flex-start;
+`;
