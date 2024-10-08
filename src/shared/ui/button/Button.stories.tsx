238fa33ae22e5231f4c72801bad4ebfca916388f
onejuneee <onejuneee721@gmail.com>
2024-10-09 03:15:18 +0900

    test : 변경된 button 컴포넌트 스토리북 테스트 코드 추가

--- src/shared/ui/button/Button.stories.tsx
+++ src/shared/ui/button/Button.stories.tsx
@@ -1,20 +1,33 @@
 import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import Button from './Button';
 import IcBookmark from '@/shared/assets/svg/IcBookmark.svg?react';
+import IcChat from '@/shared/assets/svg/Icchat.svg?react';
+import IcChatGithub from '@/shared/assets/svg/IcChatGithub.svg?react';
+import IcChatStart from '@/shared/assets/svg/IcChatStart.svg?react';
+import IcClose from '@/shared/assets/svg/Icclose.svg?react';
 import IcList from '@/shared/assets/svg/IcList.svg?react';
-import { Button } from '@/shared/ui/button/Button';
+import IcLoginGithub from '@/shared/assets/svg/IcLoginGithub.svg?react';
+import IcPhoto from '@/shared/assets/svg/IcPhoto.svg?react';
+import IcSend from '@/shared/assets/svg/IcSend.svg?react';
+import { StoryContainer, SectionContainer, RowContainer } from '@/shared/ui/storybook/story.style';
 
 const meta: Meta<typeof Button> = {
-  title: 'shared/Button',
+  title: 'Components/Button',
   component: Button,
+  argTypes: {
+    variant: {
+      options: ['primary', 'circle', 'inactivePrimary', 'inactiveSecondary'],
+      control: { type: 'radio' },
+    },
+    size: {
+      options: ['sm', 'lg'],
+      control: { type: 'radio' },
+    },
+    children: { control: { type: 'text' } },
+  },
   parameters: {
     layout: 'centered',
-  },
-  argTypes: {
-    children: { control: 'text' },
-    onClick: { action: 'clicked' },
-  },
-  args: {
-    children: '버튼',
   },
 };
 
@@ -24,28 +37,64 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     children: '버튼',
+    variant: 'primary',
+    size: 'sm',
   },
 };
 
 export const All: Story = {
   render: (args) => (
-    <div>
-      <h3>모든 버튼</h3>
-      <div style={{ marginBottom: '1rem' }}>
-        <Button {...args} icon={<IcBookmark />} iconType="bookmark">
-          북마크
-        </Button>
-      </div>
-      <div style={{ marginBottom: '1rem' }}>
-        <Button {...args} icon={<IcList />} iconType="list">
-          목록으로
-        </Button>
-      </div>
-      <div style={{ marginBottom: '1rem' }}>
-        <Button {...args} disabled>
-          모집 완료
-        </Button>
-      </div>
-    </div>
+    <StoryContainer>
+      <SectionContainer>
+        <h3>기본 버튼</h3>
+        <RowContainer>
+          <Button variant="primary" size="sm">
+            버튼
+          </Button>
+          <Button variant="primary" size="sm" icon={<IcBookmark />}>
+            북마크
+          </Button>
+          <Button variant="primary" size="sm" icon={<IcList />}>
+            목록으로
+          </Button>
+          <Button variant="primary" size="sm" icon={<IcLoginGithub />}>
+            로그인
+          </Button>
+          <Button variant="primary" size="sm" icon={<IcChatStart />}>
+            채팅하기
+          </Button>
+          <Button variant="primary" size="lg">
+            확인
+          </Button>
+        </RowContainer>
+      </SectionContainer>
+      <SectionContainer>
+        <h3>채팅 버튼</h3>
+        <RowContainer>
+          <Button variant="circle" size="lg" icon={<IcChat />} />
+          <Button variant="circle" size="lg" icon={<IcClose />} />
+        </RowContainer>
+      </SectionContainer>
+      <SectionContainer>
+        <h3>채팅 전송 버튼</h3>
+        <RowContainer>
+          <Button variant="circle" size="sm" icon={<IcSend />} />
+          <Button variant="circle" size="sm" icon={<IcPhoto />} />
+          <Button variant="circle" size="sm" icon={<IcChatGithub />} />
+        </RowContainer>
+      </SectionContainer>
+
+      <SectionContainer>
+        <h3>완료 버튼(클릭 안됨)</h3>
+        <RowContainer>
+          <Button variant="inactivePrimary" size="sm">
+            신청 완료
+          </Button>
+          <Button variant="inactiveSecondary" size="sm">
+            모집 완료
+          </Button>
+        </RowContainer>
+      </SectionContainer>
+    </StoryContainer>
   ),
 };
