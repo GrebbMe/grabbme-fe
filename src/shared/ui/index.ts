2eaef034575805ba5048b7e77652ef1981e2ce2f
onejuneee <onejuneee721@gmail.com>
2024-10-09 04:39:03 +0900

    refactor : button 컴포넌트가 export 되는 경로 수정

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,4 @@
 export { SelectList } from '@/shared/ui/select/SelectList';
 export { RemoveButton } from '@/shared/ui/button/RemoveButton';
 export { AddButton } from '@/shared/ui/button/AddButton';
-export { Button } from '@/shared/ui/button/Button';
+export { default as Button } from '@/shared/ui/button/Button';
