3351b73aa33dcaba94e985bf659fde11477e6117
minzziPark <minzzi0068@gmail.com>
2024-10-09 01:17:39 +0900

    ui : stack 컴포넌트 구현

---
+++ src/features/home/ui/StackSelectButton.tsx
@@ -0,0 +1,16 @@
+import * as S from './stackSelectButton.style';
+
+export interface ButtonProps {
+  children: string;
+  src?: string;
+  onClick?: () => void;
+}
+
+export const StackSelectButton = ({ children, src, onClick }: ButtonProps) => {
+  return (
+    <S.Container onClick={onClick}>
+      <S.StackImg src={src} alt="stack-button" />
+      <S.StackName>{children}</S.StackName>
+    </S.Container>
+  );
+};
