3a43acd801d79d4b2111d56ba79c5f45e257171a
ParkJeongJoon <parkjeong616@gmail.com>
2024-10-08 17:49:07 +0900

    refactor : selectlist 수정

--- src/shared/ui/select/SelectList.stories.tsx
+++ src/shared/ui/select/SelectList.stories.tsx
@@ -4,7 +4,7 @@ import { SelectList } from '@/shared/ui';
 import { SelectListItem, SelectListProps } from '@/shared/ui/select/SelectList';
 
 const meta: Meta<SelectListProps> = {
-  title: 'component/shared/select',
+  title: 'shared/select',
   component: SelectList,
   argTypes: {
     items: {
@@ -16,53 +16,83 @@ const meta: Meta<SelectListProps> = {
       options: ['sm', 'md', 'lg'],
       description: '드롭다운 길이',
     },
+    placeholder: {
+      control: 'text',
+      description: '선택된 값이 없을 때 보여질 문구',
+    },
   },
 };
 export default meta;
 type Story = StoryObj<SelectListProps>;
 
-export const SmallSelectList: Story = {
+export const Default: Story = {
   args: {
     items: [
-      { value: '1', display: '1-2년차' },
-      { value: '2', display: '3-5년차' },
-      { value: '3', display: '5-7년차' },
-    ],
-    size: 'sm',
-    selected: null,
-  },
-  render: function Render(args: SelectListProps) {
-    const [selected, setSelected] = useState<SelectListItem | null>(null);
-    return <SelectList {...args} selected={selected} onChange={setSelected} />;
-  },
-};
-
-export const MediumSelectList: Story = {
-  args: {
-    items: [
-      { value: '1', display: '1-2년차' },
-      { value: '2', display: '3-5년차' },
-      { value: '3', display: '5-7년차' },
-    ],
-    size: 'md',
-  },
-  render: function Render(args: SelectListProps) {
-    const [selected, setSelected] = useState<SelectListItem | null>(null);
-    return <SelectList {...args} selected={selected} onChange={setSelected} />;
-  },
-};
-
-export const LargeSelectList: Story = {
-  args: {
-    items: [
-      { value: '1', display: '1-2년차' },
-      { value: '2', display: '3-5년차' },
-      { value: '3', display: '5-7년차' },
+      { value: '1', label: '1-2년차' },
+      { value: '2', label: '3-5년차' },
+      { value: '3', label: '5-7년차' },
     ],
     size: 'lg',
+    placeholder: '선택',
   },
   render: function Render(args: SelectListProps) {
     const [selected, setSelected] = useState<SelectListItem | null>(null);
-    return <SelectList {...args} selected={selected} onChange={setSelected} />;
+    const handleItemClick = (item: SelectListItem) => {
+      setSelected(item);
+    };
+
+    return <SelectList {...args} selected={selected} onChangeSelected={handleItemClick} />;
+  },
+};
+
+export const All: Story = {
+  args: {
+    items: [
+      { value: '1', label: '1-2년차' },
+      { value: '2', label: '3-5년차' },
+      { value: '3', label: '5-7년차' },
+    ],
+    size: 'md',
+    placeholder: '선택',
+  },
+  render: function Render(args: SelectListProps) {
+    const [smallSelected, setSmallSelected] = useState<SelectListItem | null>(null);
+    const [mediumSelected, setMediumSelected] = useState<SelectListItem | null>(null);
+    const [LargeSelected, setLargeSelected] = useState<SelectListItem | null>(null);
+
+    const handleSmallSelect = (item: SelectListItem) => {
+      setSmallSelected(item);
+    };
+    const handleMediumSelect = (item: SelectListItem) => {
+      setMediumSelected(item);
+    };
+    const handleLargeSelect = (item: SelectListItem) => {
+      setLargeSelected(item);
+    };
+
+    return (
+      <>
+        <SelectList
+          {...args}
+          size={'sm'}
+          selected={smallSelected}
+          onChangeSelected={handleSmallSelect}
+        />
+        <br />
+        <SelectList
+          {...args}
+          size={'md'}
+          selected={mediumSelected}
+          onChangeSelected={handleMediumSelect}
+        />
+        <br />
+        <SelectList
+          {...args}
+          size={'lg'}
+          selected={LargeSelected}
+          onChangeSelected={handleLargeSelect}
+        />
+      </>
+    );
   },
 };
