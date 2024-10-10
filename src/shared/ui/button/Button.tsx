5399f5cc6a025938cf418b82ca14a368d7ae0343
onejuneee <onejuneee721@gmail.com>
2024-10-10 14:57:44 +0900

    refactor : forwardref 래핑 스타일 변경 및 옵셔널 props 제거

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,22 +1,19 @@
-import { ComponentPropsWithoutRef, ForwardedRef, forwardRef, ReactElement } from 'react';
+import { ComponentPropsWithoutRef, forwardRef, ReactElement } from 'react';
 import { StyledButton } from './Button.style';
 
 export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
-  variant?: 'primary' | 'circle' | 'inactivePrimary' | 'inactiveSecondary';
-  size?: 'sm' | 'lg';
+  variant: 'primary' | 'circle' | 'inactivePrimary' | 'inactiveSecondary';
+  size: 'sm' | 'lg';
   icon?: ReactElement;
 }
 
-const Button = (
-  { variant = 'primary', size = 'sm', icon, children, ...props }: ButtonProps,
-  ref: ForwardedRef<HTMLButtonElement>,
-) => {
+const Button = forwardRef<HTMLButtonElement>(({ variant, size, icon, children, ...props }, ref) => {
   return (
     <StyledButton variant={variant} size={size} icon={icon} {...props} ref={ref}>
       {icon}
       {children}
     </StyledButton>
   );
-};
+});
 
-export default forwardRef(Button);
+export default Button;
