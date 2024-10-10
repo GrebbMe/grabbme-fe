d730fa59f894557c92b342f223c5ba3198f0647e
ParkJeongJoon <parkjeong616@gmail.com>
2024-10-10 12:54:00 +0900

    ui : 레이아웃 수정

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from '@/app/layout/Footer';
 import { Header } from '@/app/layout/Header';
-import { Container } from '@/app/layout/layout.style';
+import { Container, Wrapper } from '@/app/layout/layout.style';
 
 export const Layout = () => {
   return (
     <>
       <Header />
       <Container>
-        <Outlet />
+        <Wrapper>
+          <Outlet />
+        </Wrapper>
       </Container>
       <Footer />
     </>
