b7ed2b45968131829d3d72506c065b6a6c6d8c12
ParkJeongJoon <parkjeong616@gmail.com>
2024-10-12 02:38:54 +0900

    ui : 에디터 스타일링

---
+++ src/features/post/ui/editor/Editor.tsx
@@ -0,0 +1,48 @@
+import { useRef } from 'react';
+import ReactQuill, { Quill } from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
+import { EditorWrapper } from './Editor.style';
+
+interface EditorProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const bold: any = Quill.import('formats/bold');
+bold.tagName = 'b';
+Quill.register(bold, true);
+
+const italic: any = Quill.import('formats/italic');
+italic.tagName = 'i';
+Quill.register(italic, true);
+
+const toolbarOptions = [
+  [
+    { header: [1, 2, 3, 4, 5, 6, false] },
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    { list: 'ordered' },
+    { list: 'bullet' },
+    { color: [] },
+    { align: [] },
+  ],
+];
+
+const modules = {
+  toolbar: {
+    container: toolbarOptions,
+  },
+};
+
+export const Editor = ({ value, onChange }: EditorProps) => {
+  const quillRef = useRef<ReactQuill>(null);
+
+  return (
+    <EditorWrapper>
+      <ReactQuill ref={quillRef} value={value} onChange={onChange} modules={modules} />
+    </EditorWrapper>
+  );
+};
