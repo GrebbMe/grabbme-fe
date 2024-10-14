38ea073661a98badd352120c418efc7818b99c37
ChanGyunLee <lgyn10@naver.com>
2024-10-14 09:56:32 +0900

    feat : 헤더에 소셜로그인 기능 버튼 교체

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as S from '@/app/layout/Header.style';
+import { SocialLogin } from '@/features/auth';
 import { ProfileIcon } from '@/shared/assets';
 import logo from '@/shared/assets/image/logo.png';
-import { Button, CountBox } from '@/shared/ui';
 
 export const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [profileImage, setProfileImage] = useState('');
-  // TODO: 로그인 시 사용자 이름으로 변경 , 임시 값
-  const [userName, setUserName] = useState('그랩미');
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    //! 쿠키의 accessToken이 유효한지 확인 후, 로그인 상태 설정
+  }, []);
 
   const handleLoginClick = () => {
     setIsLoggedIn(true);
@@ -28,7 +31,9 @@ export const Header = () => {
             <span>{userName} 님</span>
           </S.ProfileBox>
         ) : (
-          <S.LoginBox></S.LoginBox>
+          <S.LoginBox>
+            <SocialLogin />
+          </S.LoginBox>
         )}
       </S.GridWrapper>
     </S.HeaderContainer>
