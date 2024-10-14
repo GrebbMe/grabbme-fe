b3cbbd118e133a76d0cca5898500920f7ef2d81d
onejuneee <onejuneee721@gmail.com>
2024-10-15 04:32:20 +0900

    test : status tag 컴포넌트 스토리북 추가

---
+++ src/features/myPage/ui/StatusTag.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+import StatusTag from './StatusTag';
+import { StoryContainer, SectionContainer, RowContainer } from '@/shared/ui/storybook/story.style';
+
+const meta: Meta<typeof StatusTag> = {
+  title: 'components/shared/StatusTag',
+  component: StatusTag,
+  argTypes: {
+    status: {
+      options: ['rejected', 'accepted', 'pending'],
+      control: { type: 'radio' },
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    status: 'accepted',
+  },
+};
+
+export const All: Story = {
+  render: () => (
+    <StoryContainer>
+      <SectionContainer>
+        <h3>상태 태그 전체</h3>
+        <RowContainer>
+          <StatusTag status="rejected" />
+          <StatusTag status="accepted" />
+          <StatusTag status="pending" />
+        </RowContainer>
+      </SectionContainer>
+    </StoryContainer>
+  ),
+};
