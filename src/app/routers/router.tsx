1930930c7c0e2f92f33fefc9114b6fd4080ef5e2
onejuneee <onejuneee721@gmail.com>
2024-10-15 02:32:43 +0900

    feat : 마이페이지 분기별 라우팅 추가

--- src/app/routers/router.tsx
+++ src/app/routers/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { DashboardLayout } from '@/app/layout';
+import { DashboardLayout, MainLayout } from '@/app/layout';
 import { GithubLogin, SignUp } from '@/pages/auth/ui';
+import { MyProfile, MyPosts, MyProjects, MyBookmarks } from '@/pages/mypage';
 import { routerPath } from '@/shared';
 
 export const router = createBrowserRouter([
@@ -15,6 +16,28 @@ export const router = createBrowserRouter([
       {
         path: routerPath.SIGNUP,
         element: <SignUp />,
+      },
+      {
+        path: routerPath.MYPAGE,
+        element: <MainLayout />,
+        children: [
+          {
+            path: routerPath.MY_PROFILE,
+            element: <MyProfile />,
+          },
+          {
+            path: routerPath.MY_POSTS,
+            element: <MyPosts />,
+          },
+          {
+            path: routerPath.MY_BOOKMARKS,
+            element: <MyBookmarks />,
+          },
+          {
+            path: routerPath.MY_PROJECTS,
+            element: <MyProjects />,
+          },
+        ],
       },
     ],
   },
