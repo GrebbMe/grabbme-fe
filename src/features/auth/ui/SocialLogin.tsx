0cf3d9834e880c19a985f55a9f7ea8baf9760dd0
ChanGyunLee <lgyn10@naver.com>
2024-10-14 10:01:02 +0900

    feat : 소셜 로그인 기능 버튼 구현

--- src/features/auth/ui/SocialLogin.tsx
+++ src/features/auth/ui/SocialLogin.tsx
@@ -1,3 +1,16 @@
+import { useCallback } from 'react';
+import { Button } from '@/shared/ui';
+
 export const SocialLogin = () => {
-  return <div>SocialLogin</div>;
+  const CLIENT_ID: string = import.meta.env.VITE_GITHUB_LOGIN_CLIENT_ID;
+  const REDIRECT_URI: string = 'http://localhost:5173/';
+
+  const authroizeGithubLogin = useCallback((): void => {
+    const url: string = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=read:user user:email`;
+    window.location.href = url;
+    // CLIENT_ID가 절대로 변경되지 않을 것이라고 확신되어 esLint를 끔
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <Button onClick={authroizeGithubLogin}>로그인</Button>;
 };
