4fb6afbec1941d1d7314030908b53d8797f372c4
minzziPark <minzzi0068@gmail.com>
2024-10-15 16:14:08 +0900

    feat : api 호출 코드 분리

---
+++ src/features/home/api/top5Stack.ts
@@ -0,0 +1,14 @@
+import { StackData } from '@/features/home/model/top5Stack.model';
+
+export const fetchData = async (): Promise<StackData[] | undefined> => {
+  try {
+    const response: Response = await fetch('/data/top5Stacks.json', {
+      method: 'GET',
+    });
+
+    const data: StackData[] = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch error: ', error);
+  }
+};
