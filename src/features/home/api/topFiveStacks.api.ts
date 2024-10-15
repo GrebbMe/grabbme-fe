42eb744c2da0c4201258e07e159c1a247d4b90f2
minzziPark <minzzi0068@gmail.com>
2024-10-15 17:26:02 +0900

    typo : 제목 동일하게 변경

--- src/features/home/api/topFiveStacks.api.ts
+++ src/features/home/api/topFiveStacks.api.ts
@@ -1,4 +1,4 @@
-import { StackData } from '@/features/home/model/top5Stacks.model';
+import { StackData } from '@/features/home/model/topFiveStacks.model';
 
 export const fetchData = async (): Promise<StackData[] | undefined> => {
   try {
