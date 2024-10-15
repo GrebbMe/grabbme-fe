b1ef87e24f2ccabf7bb59eb907ea47f358dbad3f
minzziPark <minzzi0068@gmail.com>
2024-10-15 16:17:38 +0900

    refactor : 목업데이터 사용을 위한 리팩토링

--- src/features/home/ui/TopFiveStacks.tsx
+++ src/features/home/ui/TopFiveStacks.tsx
@@ -1,40 +1,25 @@
+import { useEffect, useState } from 'react';
 import * as S from './topFiveStacks.style';
+import { fetchData } from '@/features/home/api/top5Stack';
+import { StackData } from '@/features/home/model/top5Stack.model';
 
 const TopFiveStacks = () => {
-  // TODO : top5 get 요청 코드
-  // 임시 목업데이터
-  const topFiveData = [
-    {
-      id: 1,
-      stackName: 'React',
-      peopleCount: '254명',
-    },
-    {
-      id: 2,
-      stackName: 'Kotlin',
-      peopleCount: '228명',
-    },
-    {
-      id: 3,
-      stackName: 'Swift',
-      peopleCount: '200명',
-    },
-    {
-      id: 4,
-      stackName: 'Spring',
-      peopleCount: '175명',
-    },
-    {
-      id: 5,
-      stackName: 'Flutter',
-      peopleCount: '169명',
-    },
-  ];
+  // TODO : top5 axios get 요청 코드
+  const [topFiveData, setTopFiveData] = useState<StackData[] | undefined>();
+
+  useEffect((): void => {
+    const fetchHandler = async (): Promise<void> => {
+      const data: StackData[] | undefined = await fetchData();
+      setTopFiveData(data);
+    };
+
+    fetchHandler().catch((err) => console.error(err));
+  }, []);
 
   return (
     <S.TopFiveStacksContainer>
       <S.SubTitle>TOP 5 인기 스택</S.SubTitle>
-      {topFiveData.map((data) => (
+      {topFiveData?.map((data) => (
         <S.RankContainer>
           <div>
             <S.Rank>{data.id}</S.Rank>
