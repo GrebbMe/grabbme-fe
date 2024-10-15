8c8c5eb2bb3724c39e835e02bbcbc6d674b566e8
ChanGyun Lee <lgyn10@naver.com>
2024-10-15 11:52:09 +0900

    refactor : 상수값 컴포넌트 밖으로 분리 및 deps 추가

--- src/features/auth/ui/SocialLogin.tsx
+++ src/features/auth/ui/SocialLogin.tsx
@@ -2,16 +2,16 @@ import { useCallback } from 'react';
 import IcGithubFilld from '@/shared/assets/svg/IcGithubFilled.svg?react';
 import { Button } from '@/shared/ui';
 
-export const SocialLogin = () => {
-  const CLIENT_ID: string = import.meta.env.VITE_GITHUB_LOGIN_CLIENT_ID;
-  const REDIRECT_URI: string = 'http://localhost:5173/';
+const CLIENT_ID: string = import.meta.env.VITE_GITHUB_LOGIN_CLIENT_ID;
+const REDIRECT_URI: string = 'http://localhost:5173/';
 
+export const SocialLogin = () => {
   const authroizeGithubLogin = useCallback((): void => {
     const url: string = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=read:user user:email`;
     window.location.href = url;
     // CLIENT_ID가 절대로 변경되지 않을 것이라고 확신되어 esLint를 끔
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [CLIENT_ID]);
 
   return (
     <Button icon={<IcGithubFilld />} onClick={authroizeGithubLogin}>
