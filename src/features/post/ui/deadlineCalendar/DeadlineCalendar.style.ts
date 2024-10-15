45949d9475948e2426c0244133314f3ef1199f40
ChanGyun Lee <72643542+lgyn10@users.noreply.github.com>
2024-10-15 16:27:24 +0900

    [HOTFIX] 모집 마감일 UI 에러 수정  (#74)
    * fix : 버튼 ui 에러 수정
    
    * ui : 캘린더 모달창 위치 조정
    
    * ui : 모집 마감일 인풋창 배경색 흰색으로 설정
    
    * ui : 캘린더 모달창을 인풋창 하단 고정
    
    * typo : 불필요한 import 문 제거

--- src/features/post/ui/deadlineCalendar/DeadlineCalendar.style.ts
+++ src/features/post/ui/deadlineCalendar/DeadlineCalendar.style.ts
@@ -10,6 +10,7 @@ const DatePickerContainer = styled.div`
   border-radius: 0.8rem;
   width: 28.8rem;
   height: 4.8rem;
+  background-color: ${({ theme }) => theme.color.WHITE};
 `;
 
 const StyledDatePicker = styled(DatePicker)`
@@ -42,8 +43,8 @@ const DatePickerLayout = styled.div`
 
   .react-datepicker-popper {
     position: absolute;
-    left: 1.2rem !important;
-    top: 1rem !important;
+    left: -1.6rem !important;
+    top: 1.6rem !important;
 
     .react-datepicker__triangle {
       display: none;
@@ -143,6 +144,7 @@ const DatePickerLayout = styled.div`
             &.react-datepicker__day--selected {
               background-color: ${({ theme }) => theme.color.PRIMARY_BLUE} !important;
               width: 3.2rem;
+
               border-radius: 50%;
               color: ${({ theme }) => theme.color.WHITE};
               pointer-events: none;
