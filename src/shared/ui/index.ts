1bd3cb9ed5851c94cdd19231fb04a232c21698a9
onejuneee <onejuneee721@gmail.com>
2024-10-16 00:59:00 +0900

    feat : countlabel export 설정

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,4 @@ export { Select } from '@/shared/ui/select/Select';
 export { RemoveButton } from '@/shared/ui/button/RemoveButton';
 export { AddButton } from '@/shared/ui/button/AddButton';
 export { default as Button } from '@/shared/ui/button/Button';
+export { default as CountLabel } from '@/shared/ui/countLabel/CountLabel';
