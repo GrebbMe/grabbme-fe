b0bd15b0bb9a199aee04c1a5722913aad27ad6f3
onejuneee <onejuneee721@gmail.com>
2024-10-17 03:56:06 +0900

    typo : contentcardtype 인터페이스 명 변경

--- src/widgets/contentCard/ContentCard.tsx
+++ src/widgets/contentCard/ContentCard.tsx
@@ -3,7 +3,7 @@ import * as S from './ContentCard.style';
 import { IcBookmarkSmall, IcChatSmall, IcViewSmall } from '@/shared/assets';
 import { CountLabel } from '@/shared/ui';
 
-export type ContentCardType = 'A' | 'B' | 'C';
+export type ContentCardType = 'GrabbzoneCard' | 'RecruitCard' | 'MyProjectCard';
 
 interface ContentCardProps {
   title: string;
@@ -26,14 +26,14 @@ const ContentCard = ({
   role,
   type,
 }: ContentCardProps) => {
-  const hasBorder = type !== 'C';
+  const hasBorder = type !== 'MyProjectCard';
 
   return (
     <S.CardContainer>
       <S.CardWrapper>
         <S.TitleWrapper>
           <S.TitleBox>{title}</S.TitleBox>
-          {type === 'A' && name && role && (
+          {type === 'GrabbzoneCard' && name && role && (
             <S.InfoWrapper>
               <S.InfoBox>{name}</S.InfoBox>
               <S.DotBox>•</S.DotBox>
