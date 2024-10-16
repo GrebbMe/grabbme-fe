ad494adf5bf895560f0a93235a1a934d2507f972
onejuneee <onejuneee721@gmail.com>
2024-10-16 22:07:57 +0900

    ui : 채팅 버블 퍼블리싱

---
+++ src/features/chat/ui/ChatMessage.tsx
@@ -0,0 +1,44 @@
+import * as S from './ChatMessage.style';
+import { ChatBubble } from './ChatMessage.style';
+import { IcDefaultProfile } from '@/shared/assets';
+
+interface ChatMessageProps {
+  message: string;
+  time: string;
+  type: 'received' | 'sent';
+  profileImageUrl?: string;
+}
+
+const ChatMessage = ({ message, time, type, profileImageUrl }: ChatMessageProps) => {
+  return (
+    <S.MessageContainer type={type}>
+      {type === 'received' && (
+        <>
+          <S.ProfileWrapper>
+            {profileImageUrl ? (
+              <S.ProfileImage src={profileImageUrl} alt="profile" />
+            ) : (
+              <S.DefaultProfileIcon>
+                <IcDefaultProfile />
+              </S.DefaultProfileIcon>
+            )}
+            <S.ChatBubble type={type}>
+              <S.TextLabel>{message}</S.TextLabel>
+            </S.ChatBubble>
+          </S.ProfileWrapper>
+          <S.TimeLabel>{time}</S.TimeLabel>
+        </>
+      )}
+      {type === 'sent' && (
+        <>
+          <S.TimeLabel>{time}</S.TimeLabel>
+          <S.Bubble type={type}>
+            <S.TextLabel>{message}</S.TextLabel>
+          </S.Bubble>
+        </>
+      )}
+    </S.MessageContainer>
+  );
+};
+
+export default ChatMessage;
