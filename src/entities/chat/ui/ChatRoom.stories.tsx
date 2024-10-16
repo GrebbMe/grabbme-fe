ab276b29ca0c1062ebae032f3f3a4af5daefb616
onejuneee <onejuneee721@gmail.com>
2024-10-17 00:36:26 +0900

    test : 채팅방 스토리북 추가

---
+++ src/entities/chat/ui/ChatRoom.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ChatRoom from './ChatRoom';
+import { StoryContainer, SectionContainer } from '@/shared/ui/storybook/story.style';
+
+const meta: Meta<typeof ChatRoom> = {
+  title: 'components/widgets/ChatRoom',
+  component: ChatRoom,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof ChatRoom>;
+
+export const Default: Story = {
+  render: () => (
+    <StoryContainer>
+      <SectionContainer>
+        <ChatRoom />
+      </SectionContainer>
+    </StoryContainer>
+  ),
+};
+
+export const All: Story = {
+  render: () => (
+    <StoryContainer>
+      <SectionContainer>
+        <h3>전체</h3>
+        <ChatRoom />
+      </SectionContainer>
+    </StoryContainer>
+  ),
+};
