3725937b7b2a19a248873b3fd91f270168cb51a2
onejuneee <onejuneee721@gmail.com>
2024-10-16 23:04:45 +0900

    feat : 채팅방 읽음과 읽지 않음을 구별하는 함수 선언

---
+++ src/entities/chat/lib/sortChatRooms.ts
@@ -0,0 +1,10 @@
+import type { ChatRoom } from '@/entities/chat/ui/ChatRoomList';
+
+export const SortChatRooms = (rooms: ChatRoom[]): ChatRoom[] => {
+  return rooms.sort((a, b) => {
+    if (a.isRead === b.isRead) {
+      return new Date(b.time).getTime() - new Date(a.time).getTime();
+    }
+    return a.isRead ? 1 : -1;
+  });
+};
