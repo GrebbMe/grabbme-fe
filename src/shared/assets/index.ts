362bff036eadecddefe41e7c30771835b89c407b
onejuneee <onejuneee721@gmail.com>
2024-10-16 21:12:37 +0900

    feat : 기본 프로필 아이콘 추가

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -16,5 +16,6 @@ export { default as IcLoginGithub } from '@/shared/assets/svg/IcLoginGithub.svg?
 export { default as IcPhoto } from '@/shared/assets/svg/IcPhoto.svg?react';
 export { default as IcSend } from '@/shared/assets/svg/IcSend.svg?react';
 export { default as IcSendActive } from '@/shared/assets/svg/IcSendActive.svg?react';
+export { default as IcDefaultProfile } from '@/shared/assets/svg/IcDefaultProfile.svg?react';
 export { default as IcSmallChat } from '@/shared/assets/svg/IcSmallChat.svg?react';
 export { default as IcView } from '@/shared/assets/svg/IcView.svg?react';
