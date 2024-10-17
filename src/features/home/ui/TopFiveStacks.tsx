cbf49fd33695e06cd3a8ecd7786bc7803b225157
minzziPark <86656147+minzziPark@users.noreply.github.com>
2024-10-17 14:42:32 +0900

    [UI] top5 stack UI 구현하기 (#87)
    * ui : topfivestacks 구현
    
    * test : 스토리북 작성
    
    * refactor : 목업 데이터 파일 분리
    
    * feat : api 호출 코드 분리
    
    * feat : stackdata model 생성
    
    * refactor : 목업데이터 사용을 위한 리팩토링
    
    * typo : api 파일명 변경
    
    * typo : api 파일명 변경
    
    * typo : asyncfn 이름 변경
    
    * refactor : api 호출방법 promise 체이닝으로 변경
    
    * typo : 파일명 통일되게 변경
    
    * typo : 제목 동일하게 변경
    
    * typo : json id rank로 변경
    
    * typo : 함수명 변경
    
    * typo : 오타 수정
    
    * typo : 모델 파일명 변경
    
    * refactor : 피드백 반영

---
+++ src/features/home/ui/TopFiveStacks.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import * as S from './topFiveStacks.style';
+import { getTopFiveStack } from '@/features/home/api/topFiveStacks.api';
+import { StackData } from '@/features/home/model/stack.model';
+
+const TopFiveStacks = () => {
+  // TODO : top5 axios get 요청 코드
+  const [topFiveData, setTopFiveData] = useState<StackData[] | undefined>();
+
+  useEffect((): void => {
+    getTopFiveStack()
+      .then(setTopFiveData)
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <S.TopFiveStacksContainer>
+      <S.SubTitle>TOP 5 인기 스택</S.SubTitle>
+      {topFiveData?.map(({ rank, stackName, peopleCount }) => (
+        <S.RankContainer key={rank}>
+          <div>
+            <S.Rank>{rank}</S.Rank>
+            <S.StackName>{stackName}</S.StackName>
+          </div>
+          <S.PeopleCount>{peopleCount}</S.PeopleCount>
+        </S.RankContainer>
+      ))}
+    </S.TopFiveStacksContainer>
+  );
+};
+
+export default TopFiveStacks;
