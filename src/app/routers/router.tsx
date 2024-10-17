c66fea7b0e27220e7b26b481d80da7fd3d56d37a
onejuneee <onejuneee721@gmail.com>
2024-10-17 23:05:33 +0900

    refactor : 마이페이지 라우팅 경로를 독립적으로 지정

--- src/app/routers/router.tsx
+++ src/app/routers/router.tsx
@@ -16,27 +16,27 @@ export const router = createBrowserRouter([
       },
       { path: routerPath.PROJECT, element: <Project /> },
       { path: routerPath.GRABB_ZONE, element: <GrabbZone /> },
+    ],
+  },
+  {
+    path: routerPath.MYPAGE,
+    element: <MainLayout />,
+    children: [
       {
-        path: routerPath.MYPAGE,
-        element: <MainLayout />,
-        children: [
-          {
-            path: routerPath.MY_PROFILE,
-            element: <MyProfile />,
-          },
-          {
-            path: routerPath.MY_POSTS,
-            element: <MyPosts />,
-          },
-          {
-            path: routerPath.MY_BOOKMARKS,
-            element: <MyBookmarks />,
-          },
-          {
-            path: routerPath.MY_PROJECTS,
-            element: <MyProjects />,
-          },
-        ],
+        path: routerPath.MY_PROFILE,
+        element: <MyProfile />,
+      },
+      {
+        path: routerPath.MY_POSTS,
+        element: <MyPosts />,
+      },
+      {
+        path: routerPath.MY_BOOKMARKS,
+        element: <MyBookmarks />,
+      },
+      {
+        path: routerPath.MY_PROJECTS,
+        element: <MyProjects />,
       },
     ],
   },
