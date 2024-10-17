66046ae7a5c2f03c6bcb7d2dfd11cefa648fc9ab
onejuneee <onejuneee721@gmail.com>
2024-10-17 23:00:14 +0900

    feat : 추가 작성된 컴포넌트 export 경로 모듈화

--- src/features/myPage/index.ts
+++ src/features/myPage/index.ts
@@ -1,1 +1,1 @@
-export { default as SideBar } from './ui/Sidebar';
+export { default as Sidebar } from './ui/Sidebar';
--- src/widgets/user/index.ts
+++ src/widgets/user/index.ts
@@ -1,1 +1,2 @@
-// user/index.ts
+export { default as ProfileDetail } from './ui/ProfileDetail';
+export { default as UserProfile } from './ui/UserProfile';
